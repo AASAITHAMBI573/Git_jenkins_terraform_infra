73f15c5d9ded84e1f71174d022c85be9681e760c
root <root@ip-172-31-29-81.us-east-2.compute.internal>
2023-05-21 19:37:33 +0000

    first commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAZKZIUZ2DATRSDMXW"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-2"
+  }
